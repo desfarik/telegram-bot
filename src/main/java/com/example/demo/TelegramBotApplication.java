7c978b00ae2e2087a3edddb8e3fc224a729f0de0
desfarik <desfarik@gmail.com>
2018-11-18 17:38:57 +0300

    Init commit

--- src/main/java/com/example/demo/TelegramBotApplication.java
+++ src/main/java/com/example/demo/TelegramBotApplication.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
 
 @SpringBootApplication
 public class TelegramBotApplication {
     public static void main(String[] args) {
         ApiContextInitializer.init();
+        DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+        String host = "0.0.0.0";
+        int port = Integer.valueOf(System.getenv("PORT"));
+        botOptions.setProxyPort(port);
+        botOptions.setProxyHost(host);
         SpringApplication.run(TelegramBotApplication.class, args);
     }
 
